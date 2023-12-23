934b3203decb3ade3222944645d45ec3c1d8a9b3
Maria <globalkmaria@gmail.com>
2023-12-24 07:19:10 +0900

    Edit : clock to use more correct time

--- src/app/hooks/useClock.tsx
+++ src/app/hooks/useClock.tsx
@@ -3,17 +3,35 @@
 import { useState, useEffect } from 'react';
 
 function useClock() {
+  const [mount, setMount] = useState(true);
   const [currentTime, setCurrentTime] = useState(Date.now());
 
   useEffect(() => {
+    if (mount) return;
     const timerID = setInterval(() => {
       setCurrentTime(Date.now());
     }, 60000);
 
     return () => clearInterval(timerID);
+  }, [mount]);
+
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setMount(false);
+      setCurrentTime(Date.now());
+    }, countTimeUntilNextTick());
+    return () => clearTimeout(timerID);
   }, []);
 
   return currentTime;
 }
 
 export default useClock;
+
+const countTimeUntilNextTick = () => {
+  const currentTime = Date.now();
+  const nextMinuteTime = Math.ceil(currentTime / 60000) * 60000;
+  const timeLeftToNextMinute = nextMinuteTime - currentTime;
+
+  return timeLeftToNextMinute;
+};
