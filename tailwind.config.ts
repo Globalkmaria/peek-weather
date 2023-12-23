57f98387c77e17a435b4c5df9126e252eacae256
Maria <globalkmaria@gmail.com>
2023-12-23 21:57:57 +0900

    Add : close and open-scroll animations to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,20 @@ const config: Config = {
         primary: '#DEC3A0',
         secondary: '#000000',
       },
+      keyframes: {
+        'open-scroll': {
+          '0%': { 'grid-template-rows': '0fr' },
+          '100%': { 'grid-template-rows': '1fr' },
+        },
+        'close-scroll': {
+          '0%': { 'grid-template-rows': '1fr' },
+          '100%': { 'grid-template-rows': '0fr' },
+        },
+      },
+      animation: {
+        'open-scroll': 'open-scroll 0.2s ease-in-out forwards',
+        'close-scroll': 'close-scroll 0.2s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
