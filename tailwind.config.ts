4ca979c96e10957ec9b1a614274bef82cfb08adc
Maria <globalkmaria@gmail.com>
2023-12-23 19:12:52 +0900

    Add : theme color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#DEC3A0',
+        secondary: '#000000',
+      },
+    },
   },
   plugins: [],
 };
