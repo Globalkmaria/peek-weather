b4c9cf333b5865abeb6815187e07b8645d47bfac
Maria <globalkmaria@gmail.com>
2023-12-23 22:06:46 +0900

    Add : ExpandCard Component

---
+++ src/app/components/ExpandCard.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { MdExpandLess, MdExpandMore } from 'react-icons/md';
+
+interface ExpandCardProps {
+  children?: ReactNode;
+  open: boolean;
+}
+
+const ExpandCard = ({ open, children }: ExpandCardProps) => {
+  const openClassName = open ? 'animate-close-scroll' : 'animate-open-scroll';
+  return (
+    <div>
+      <div className={`grid overflow-hidden ${openClassName}`}>
+        <div className='min-h-0'>{children}</div>
+      </div>
+      <button
+        className='flex items-center justify-center w-full text-2xl'
+        type='button'
+        aria-label={`expand ${open ? 'close' : 'open'} button`}
+      >
+        {open ? <MdExpandLess /> : <MdExpandMore />}
+      </button>
+    </div>
+  );
+};
+
+export default ExpandCard;
